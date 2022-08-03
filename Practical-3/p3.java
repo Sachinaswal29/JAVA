99c9f8d10a9ea59abbfd8bbf1b4bf2e93eb54855
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-08-03 11:11:23 +0530

    Add files via upload

---
+++ Practical-3/p3.java
@@ -0,0 +1,7 @@
+class Xyz{
+     public static void main(String args[]){
+     int x=Integer.parseInt(args[0]);
+     int y=Integer.parseInt(args[1]);
+     System.out.println("Sum is:"+ (x+y));
+     }
+}
