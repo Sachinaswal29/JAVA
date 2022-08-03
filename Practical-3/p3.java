1ca08332b2559845edad0d78a6536f0372cf57e4
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-08-03 11:13:40 +0530

    Update p3.java

--- Practical-3/p3.java
+++ Practical-3/p3.java
@@ -1,6 +1,6 @@
 class Xyz{
      public static void main(String args[]){
-     int x=Integer.parseInt(args[0]);
+     int x=Integer.parseInt(args[0]);  //Taking input through Command line argument
      int y=Integer.parseInt(args[1]);
      System.out.println("Sum is:"+ (x+y));
      }
