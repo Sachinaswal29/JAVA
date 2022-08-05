44705031b75a27d15773918e9814a94e59820cf6
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-08-05 11:59:35 +0530

    Update p7.java

--- Practical-7/p7.java
+++ Practical-7/p7.java
@@ -2,16 +2,16 @@ class Append{
   public static void main(String args[]){  
   StringBuffer sb=new StringBuffer("HELLO ");  
   sb.append("Java");              //now original string is changed  
-  System.out.println(sb);         // Hello Java  
+  System.out.println(sb);         // HELLO Java  
   }  
 }  
 
 
 class Insert{  
   public static void main(String args[]){  
-  StringBuffer sb=new StringBuffer("HELLO ");  
+  StringBuffer sb=new StringBuffer("HELLO");  
   sb.insert(1,"Java");          //now original string is changed  
-  System.out.println(sb);     // HJavaello  
+  System.out.println(sb);     // HJavaELLo  
   }  
 } 
  
@@ -20,7 +20,7 @@ class Replace{
   public static void main(String args[]){  
   StringBuffer sb=new StringBuffer("HELLO");  
   sb.replace(1,3,"Java");         // replace from 1 to before 3 index
-  System.out.println(sb);        // HJavalo  
+  System.out.println(sb);        // HJavaLO  
   }  
 }  
 
@@ -30,7 +30,7 @@ class Delete{
   public static void main(String args[]){  
   StringBuffer sb=new StringBuffer("HELLO");  
   sb.delete(1,3);                     // delete from 1 to before 3 index
-  System.out.println(sb);        // Hlo  
+  System.out.println(sb);        // HLO  
   }  
 }  
 
