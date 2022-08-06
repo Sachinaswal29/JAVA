dfbe1bbfb46af45a65c725f39cfbc95f0f9da032
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-08-06 13:14:35 +0530

    Add files via upload

---
+++ Practical-8/p8.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Student {
+   int id;
+   String name;
+   String branch;
+   String city;
+   String university;
+
+   public Student(int id, String name) {
+      this.id = id;
+      this.name = name;
+   }
+
+   public Student(int id, String name, String branch) {
+      this(id, name);
+      this.branch = branch;
+
+   }
+
+   public Student(int id, String name, String branch, String city) {
+      this(id, name, branch);
+      this.city = city;
+   }
+
+   public Student(int id, String name, String branch, String city, String university) {
+      this(id, name, branch, city);
+      this.university = university;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public void showDetails() {
+      System.out.println("\n"+"Details are:");
+      System.out.println("ID -> " + id);
+      System.out.println("Name -> " + name);
+      System.out.println("Branch -> " + branch);
+      System.out.println("City -> " + city);
+      System.out.println("University -> " + university);
+   }
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter ID : ");
+      int id = scanner.nextInt();
+      System.out.print("Enter Name : ");
+      String name = scanner.next();
+      System.out.print("Enter Branch : ");
+      String branch = scanner.next();
+      System.out.print("Enter City : ");
+      String city = scanner.next();
+      System.out.print("Enter University : ");
+      String uni = scanner.next();
+      Student emp = new Student(id, name, branch, city, uni);
+      emp.showDetails();
+   }
+}
