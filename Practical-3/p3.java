341f572473af7504315df68546c79e1812d99320
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-16 00:30:14 +0530

    Update p3.java

--- Practical-3/p3.java
+++ Practical-3/p3.java
@@ -1,3 +1,4 @@
+/* Write a program to sum two numbers. Here inputs are provided through command line argument. */
 class Xyz{
      public static void main(String args[]){
      int x=Integer.parseInt(args[0]);  //Taking input through Command line argument
