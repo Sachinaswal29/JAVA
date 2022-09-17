42e6ba449ebdaf957ac555395c57d7beef9ed636
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-17 22:38:35 +0530

    Update p6.java

--- Practical-6/p6.java
+++ Practical-6/p6.java
@@ -1,3 +1,13 @@
+/*Apply following functions on the String "Java".
+(i) Try to concat "Welcome" and write down your observation.
+(ii) Find character at index 1
+(iii) Find index of first 'a'.
+(iv) Find index of second 'a'
+(v) Compare "Java" to "JAVA"
+(vi) Compare "Java" to "JAVA" ignoring the case
+(vii) Find the index of first 'a' from last
+*/
+
 class Str{ 
 public static void main(String args[]){
     String s="Java";
