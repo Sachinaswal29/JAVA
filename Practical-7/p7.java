532f29640f28105818a3ecc14b999afdf2a55931
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-17 22:41:53 +0530

    Update p7.java

--- Practical-7/p7.java
+++ Practical-7/p7.java
@@ -1,3 +1,11 @@
+ /*Apply following functions on StringBuffer object "HELLO"
+(i) Append "Java"
+(ii) Insert "Java" at index 1
+(iii) Replace with "Java" with characters between index 1 to 2
+(iv) Delete characters between index 1 and 2 
+(v) Reverse the string "HELLO"
+*/
+
 class Append{  
   public static void main(String args[]){  
   StringBuffer sb=new StringBuffer("HELLO ");  
