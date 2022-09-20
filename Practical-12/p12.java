c9df363d3f055eae66f69f02d62388669d743338
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-20 16:17:30 +0530

    Update p12.java

--- Practical-12/p12.java
+++ Practical-12/p12.java
@@ -1,3 +1,6 @@
+/*Create a class “Bank” having method getRateOfInterest(). Create child classes as HDFC, SBI and PNB and override 
+getRateOfInterest() and return interest rates as 4.0, 4.5 and 5% correspondingly. */
+
 class Bank {
     double roi;
 
