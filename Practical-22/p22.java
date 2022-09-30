a6b6205b8370f35a82809bca8284fda95cb58e68
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-30 22:33:06 +0530

    Update p22.java

--- Practical-22/p22.java
+++ Practical-22/p22.java
@@ -1,3 +1,9 @@
+/* Write a class Display having synchronized void wish(String) methods that wishes hello to given string name. 
+Between printing hello and provided string name apply delay of 500 milliseconds. Suppose multiple threads are there and they are 
+trying to access this wish() method concurrently on different objects then irregular output will be there. Write this application 
+in such a way so that output becomes regular. 
+*/
+
 class Disp {
     public static synchronized void wish(String s) {
         for (int i = 1; i <= 5; i++) {
