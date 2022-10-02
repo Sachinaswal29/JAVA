bfe03ccba89c75f39f1b2ebdb573f4eccaeb961e
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-10-02 10:47:46 +0530

    Update p26.java

--- Practical-26/p26.java
+++ Practical-26/p26.java
@@ -1,3 +1,8 @@
+/* Write a JFrame having three textfields. The first two textfields refers to two numbers on which sum or subtraction will happen. 
+The third textfield will show the result. There are two buttons “SUM” and “SUBTRACTION”. Once user will click on sum or subtraction buttons 
+then the corresponding result will be displayed in result field. 
+*/
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
